692f36e30578bfcf9ec592adfbd0bafa2513f2b7
Simon Ekvy <simon.ekvy@gmail.com>
2024-12-14 22:57:42 +0100

    Remove unused dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,6 @@ kotlin {
                 implementation(libs.jetbrains.markdown)
                 implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview)
-                implementation(libs.androidx.lifecycle.viewmodel)
                 implementation(libs.androidx.lifecycle.runtime.compose)
             }
         }
